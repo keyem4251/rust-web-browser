bca2b5ca68b68cd95f5f916fc1345d71700f6658
keyem4251 <kisk.emura@gmail.com>
2024-12-14 18:01:42 +0900

    impl attribute

---
+++ web_browser_core/src/renderer/html/token.rs
@@ -0,0 +1,43 @@
+use alloc::{string::String, vec::Vec};
+
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct HtmlTokenizer {
+    state: State,
+    pos: usize,
+    reconsume: bool,
+    latest_token: Option<HtmlToken>,
+    input: Vec<char>,
+    buf: String,
+}
+
+impl HtmlTokenizer {
+    pub fn new(html: String) -> Self {
+        Self {
+            state: State::Data,
+            pos: 0,
+            reconsume: false,
+            latest_token: None,
+            input: html.chars().collect(),
+            buf: String::new(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum HtmlToken {
+    // 開始タグ
+    StartTag {
+        tag: String,
+        self_closing: bool,
+        attributes: Vec<Attribute>,
+    },
+    // 終了タグ
+    EndTag {
+        tag: String,
+    },
+    // 文字
+    Char(char),
+    // ファイルの終了（End Of File）
+    Eof,
+}
